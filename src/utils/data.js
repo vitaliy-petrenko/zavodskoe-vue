07172eef1d71ddd097205c2ee5f394a7102de2b9
Vitaliy Petrenko <vitaliy@promorepublic.com>
2018-10-24 14:26:02 +0300

    add better colors for ways

--- src/utils/data.js
+++ src/utils/data.js
@@ -16,13 +16,21 @@ export const ADMIN_TYPES = {
 };
 
 export const COLORS = {
-  'kindergarten': 'rgb(118,137,0)',
-  'school': 'rgb(87,168,0)',
-  'hospitals': 'rgb(80,175,0)',
-  'cultural_institutions': 'rgb(132,123,0)',
-  'parking': 'rgb(150,105,0)',
-  'market': 'rgb(29,226,0)',
-  'stop': 'rgb(81,174,0)'
+  // 'kindergarten': 'rgb(118,137,0)',
+  // 'school': 'rgb(87,168,0)',
+  // 'hospitals': 'rgb(80,175,0)',
+  // 'cultural_institutions': 'rgb(132,123,0)',
+  // 'parking': 'rgb(150,105,0)',
+  // 'market': 'rgb(29,226,0)',
+  // 'stop': 'rgb(81,174,0)',
+
+  'kindergarten': 'rgb(19,110,137)',
+  'school': 'rgb(82,161,199)',
+  'hospitals': 'rgb(109,43,43)',
+  'cultural_institutions': 'rgb(71,192,168)',
+  'parking': 'rgb(192,71,95)',
+  'market': 'rgb(95,192,71)',
+  'stop': 'rgb(71,95,192)'
 };
 
 export const META_FIELDS = ['builder'];
